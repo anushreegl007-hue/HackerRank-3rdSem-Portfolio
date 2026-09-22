21a81b8b847e905c2e43d604d3335431837f2034
anushreegl007-hue <anushreegl007@gmail.com>
2026-09-22 11:59:07 +0530

    Update solution.py

--- Sparse-Arrays/solution.py
+++ Sparse-Arrays/solution.py
@@ -1,1 +1,7 @@
 
+def matchingStrings(stringList, queries):
+    counts = {}
+    for s in stringList:
+        counts[s] = counts.get(s, 0) + 1
+        
+    return [counts.get(q, 0) for q in queries]
