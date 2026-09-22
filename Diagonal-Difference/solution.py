b5140fb0bb6aa4dd78288c59af0dbe2bdfcf5cb9
anushreegl007-hue <anushreegl007@gmail.com>
2026-09-22 11:57:49 +0530

    Update solution.py

--- Diagonal-Difference/solution.py
+++ Diagonal-Difference/solution.py
@@ -1,1 +1,11 @@
 
+def diagonalDifference(arr):
+    primary_sum = 0
+    secondary_sum = 0
+    n = len(arr)
+    
+    for i in range(n):
+        primary_sum += arr[i][i]
+        secondary_sum += arr[i][n - 1 - i]
+        
+    return abs(primary_sum - secondary_sum)
